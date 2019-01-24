fc220848ca810125fcd47cdccb6cc47517160276
Innocent Tauzeni <telthem.kerlyeen@gmail.com>
2019-01-24 15:51:21 +0200

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:vtsproject/mapview.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -26,11 +27,6 @@ class Dashboard extends StatelessWidget {
                 accountName: new Text("Innocent Tauzeni"),
                 accountEmail: new Text("datatelthem@gmail.com"),
               ),
-              // new ListTile(
-              //   trailing: Icon(Icons.check),
-              //      title: Text('Item 1'),
-              //      onTap: () { },
-              //   )
             ],
           ),
         ),
@@ -54,42 +50,50 @@ class TheGridView {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         verticalDirection: VerticalDirection.down,
+      
         children: <Widget>[
-             GestureDetector(
-               child: Icon(
-                icon,
-              size: 60.0,
-             ),
-              onTap: (){
-                if (name=="MapView"){
-                  
-                }
-              },
-                        ),
-                        Center(child: Text(name))
-                      ],
-                    ),
-                  );
-                }
-              
-                GridView build() {
-                  return GridView.count(
-                    primary: true,
-                    padding: EdgeInsets.all(1.0),
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.0,
-                    mainAxisSpacing: 1.0,
-                    crossAxisSpacing: 1.0,
-                    children: <Widget>[
-                      
-                      makeGridCell('Vehicles', Icons.local_car_wash),
-                      makeGridCell('Reports', Icons.report),
-                      makeGridCell('MapView', Icons.map),
-                      makeGridCell('Driver', Icons.person),
-                      makeGridCell('Gallery', Icons.photo_camera)
-                    ],
-                  );
-                }
-              
-               
+          RaisedButton(
+            color: Colors.white,
+            elevation: 0.0,
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Icon(icon,size:60.0),
+                ),
+                Center(
+                  child: Text(name),
+                )
+              ],
+            ),
+            onPressed: (){
+              print("pressed me");
+            },
+          )
+        ],
+           
+      
+        
+      ),
+    );
+  }
+
+  GridView build() {
+    return GridView.count(
+      primary: true,
+      padding: EdgeInsets.all(1.0),
+      crossAxisCount: 2,
+      childAspectRatio: 1.0,
+      mainAxisSpacing: 1.0,
+      crossAxisSpacing: 1.0,
+      children: <Widget>[
+        makeGridCell('Vehicles', Icons.local_car_wash),
+        makeGridCell('Reports', Icons.report),
+        makeGridCell('MapView', Icons.map),
+        makeGridCell('Driver', Icons.person),
+        makeGridCell('Gallery', Icons.photo_camera),
+        makeGridCell('Tracking', Icons.trending_up)
+
+      ],
+    );
+  }
 }
